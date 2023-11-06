5c53ce22a261fdf07a533485b28f8ecb6e6e48da
Miri <azer535@gmail.com>
2023-11-06 11:14:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/loginapplication/LoginScreen.kt
@@ -0,0 +1,33 @@
+package com.example.loginapplication
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            textAlign = TextAlign.Center,
+            text = "Войти в аккаунт",
+            fontSize = 80.dp,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
---
+++ app/src/main/java/com/example/loginapplication/ui/theme/Dimensions.kt
@@ -0,0 +1,26 @@
+package com.example.loginapplication.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Immutable
+data class Dimensions(
+    val dp1: Dp = 1.dp,
+    val dp2: Dp = 2.dp,
+    val dp3: Dp = 3.dp,
+    val dp4: Dp = 4.dp,
+    val dp5: Dp = 5.dp,
+    val dp6: Dp = 6.dp,
+    val dp7: Dp = 7.dp,
+    val dp8: Dp = 8.dp,
+    val dp9: Dp = 9.dp,
+    val dp10: Dp = 10.dp,
+    val dp12: Dp = 12.dp,
+    val dp14: Dp = 14.dp,
+    val dp16: Dp = 16.dp,
+    val dp18: Dp = 18.dp,
+    val dp20: Dp = 20.dp,
+    val dp22: Dp = 22.dp,
+    val dp24: Dp = 24.dp,
+)
